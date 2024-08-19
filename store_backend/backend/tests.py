cdd33ab0008847490746b8b3176d5b3ee733d73f
Rovikido <oleksandr.zakala@gmail.com>
2024-08-19 17:16:12 +0300

    added views and tests

--- store_backend/backend/tests.py
+++ store_backend/backend/tests.py
@@ -1,3 +1,192 @@
-from django.test import TestCase
+import pytest
+from rest_framework import status
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
+import backend.models as models
+from django.urls import reverse
 
-# Create your tests here.
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture
+def user(api_client):
+    user = User.objects.create_user(username='testuser', password='testpassword')
+    api_client.force_authenticate(user=user)
+    return user
+
+
+@pytest.fixture
+def product_type():
+    return models.ProductType.objects.create(name='Electronics')
+
+
+@pytest.fixture
+def product(product_type):
+    return models.Product.objects.create(
+        name='Laptop',
+        description='A powerful laptop',
+        type=product_type,
+        price=1000,
+        ammount=10
+    )
+
+
+@pytest.fixture
+def cart(user):
+    return models.Cart.objects.create(user=user)
+
+
+@pytest.fixture
+def cart_item(cart, product):
+    return models.CartItem.objects.create(cart=cart, product=product, ammount=1)
+
+
+@pytest.mark.django_db
+class TestProductTypeViewSet:
+    def test_list_product_types(self, api_client):
+        url = reverse('ProductTypes-list')
+        response = api_client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_create_product_type(self, api_client):
+        url = reverse('ProductTypes-list')
+        data = {'name': 'Furniture'}
+        response = api_client.post(url, data, format='json')
+        assert response.status_code == status.HTTP_201_CREATED
+        assert models.ProductType.objects.count() == 1
+
+    def test_retrieve_product_type(self, api_client, product_type):
+        url = reverse('ProductTypes-detail', args=[product_type.id])
+        response = api_client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_update_product_type(self, api_client, product_type):
+        url = reverse('ProductTypes-detail', args=[product_type.id])
+        data = {'name': 'Updated Electronics'}
+        response = api_client.patch(url, data, format='json')
+        assert response.status_code == status.HTTP_200_OK
+        product_type.refresh_from_db()
+        assert product_type.name == 'Updated Electronics'
+
+    def test_delete_product_type(self, api_client, product_type):
+        url = reverse('ProductTypes-detail', args=[product_type.id])
+        response = api_client.delete(url)
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+        assert models.ProductType.objects.count() == 0
+
+
+@pytest.mark.django_db
+class TestProductViewSet:
+    def test_list_products(self, api_client):
+        url = reverse('Products-list')
+        response = api_client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_create_product(self, api_client, product_type):
+        url = reverse('Products-list')
+        data = {
+            'name': 'Tablet',
+            'description': 'A new tablet',
+            'type': product_type.id,
+            'price': 500,
+            'ammount': 5
+        }
+        response = api_client.post(url, data, format='json')
+        assert response.status_code == status.HTTP_201_CREATED
+        assert models.Product.objects.count() == 1
+
+    def test_retrieve_product(self, api_client, product):
+        url = reverse('Products-detail', args=[product.id])
+        response = api_client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_update_product(self, api_client, product):
+        url = reverse('Products-detail', args=[product.id])
+        data = {'price': 900}
+        response = api_client.patch(url, data, format='json')
+        assert response.status_code == status.HTTP_200_OK
+        product.refresh_from_db()
+        assert product.price == 900
+
+    def test_delete_product(self, api_client, product):
+        url = reverse('Products-detail', args=[product.id])
+        response = api_client.delete(url)
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+        assert models.Product.objects.count() == 0
+
+
+@pytest.mark.django_db
+class TestCartViewSet:
+    def test_list_carts(self, api_client, user, cart):
+        url = reverse('Cart-list')
+        response = api_client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data[0]['user'] == user.id
+
+    def test_create_cart(self, api_client, user):
+        url = reverse('Cart-list')
+        data = {'user': user.id}
+        response = api_client.post(url, data, format='json')
+        assert response.status_code == status.HTTP_201_CREATED
+        assert models.Cart.objects.count() == 1
+        assert models.Cart.objects.latest('id').user == user
+
+    def test_retrieve_cart(self, api_client, cart):
+        url = reverse('Cart-detail', args=[cart.id])
+        response = api_client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['user'] == cart.user.id
+
+    def test_update_cart(self, api_client, cart):
+        url = reverse('Cart-detail', args=[cart.id])
+        data = {'user': cart.user.id}  # Since user is set on creation, it might be restricted to update
+        response = api_client.patch(url, data, format='json')
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_delete_cart(self, api_client, cart):
+        url = reverse('Cart-detail', args=[cart.id])
+        response = api_client.delete(url)
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+        assert models.Cart.objects.count() == 0
+
+
+@pytest.mark.django_db
+class TestCartItemViewSet:
+    def test_list_cart_items(self, api_client, user, cart, cart_item):
+        url = reverse('CartItems-list')
+        response = api_client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data[0]['cart'] == cart.id
+
+    def test_create_cart_item(self, api_client, user, cart, product):
+        url = reverse('CartItems-list')
+        data = {
+            'cart': cart.id,
+            'product': product.id,
+            'ammount': 2
+        }
+        response = api_client.post(url, data, format='json')
+        assert response.status_code == status.HTTP_201_CREATED
+        assert models.CartItem.objects.count() == 1
+
+    def test_retrieve_cart_item(self, api_client, cart_item):
+        url = reverse('CartItems-detail', args=[cart_item.id])
+        response = api_client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_update_cart_item(self, api_client, cart_item):
+        url = reverse('CartItems-detail', args=[cart_item.id])
+        data = {'ammount': 3}
+        response = api_client.patch(url, data, format='json')
+        assert response.status_code == status.HTTP_200_OK
+        cart_item.refresh_from_db()
+        assert cart_item.ammount == 3
+
+    def test_delete_cart_item(self, api_client, cart_item):
+        url = reverse('CartItems-detail', args=[cart_item.id])
+        response = api_client.delete(url)
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+        assert models.CartItem.objects.count() == 0
--- store_backend/backend/views.py
+++ store_backend/backend/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from .models import ProductType, Product, Cart, CartItem
+from rest_framework.exceptions import PermissionDenied
+import backend.serializers as serializers
 
-# Create your views here.
+
+class ProductTypeViewSet(mixins.CreateModelMixin,
+                         mixins.RetrieveModelMixin,
+                         mixins.UpdateModelMixin,
+                         mixins.DestroyModelMixin,
+                         mixins.ListModelMixin,
+                         viewsets.GenericViewSet):
+    queryset = ProductType.objects.all()
+    serializer_class = serializers.ProductTypeSerializer
+
+
+class ProductViewSet(mixins.CreateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     mixins.ListModelMixin,
+                     viewsets.GenericViewSet):
+    queryset = Product.objects.all()
+    serializer_class = serializers.ProductSerializer
+
+
+class CartViewSet(mixins.CreateModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  mixins.ListModelMixin,
+                  viewsets.GenericViewSet):
+    serializer_class = serializers.CartSerializer
+
+    def get_queryset(self):
+        """
+        Returns the carts that belong to the current user.
+        """
+        user = self.request.user
+        return Cart.objects.filter(user=user)
+
+    def perform_create(self, serializer):
+        """
+        Saves the cart with the current user.
+        """
+        #TODO: transform os only one cart exists and gets automaticly created
+        serializer.save(user=self.request.user)
+
+
+class CartItemViewSet(mixins.CreateModelMixin,
+                      mixins.RetrieveModelMixin,
+                      mixins.UpdateModelMixin,
+                      mixins.DestroyModelMixin,
+                      mixins.ListModelMixin,
+                      viewsets.GenericViewSet):
+    serializer_class = serializers.CartItemSerializer
+
+    def get_queryset(self):
+        """
+        Returns the cart items that belong to the current user's carts.
+        """
+        user = self.request.user
+        return CartItem.objects.filter(cart__user=user)
+
+    def perform_create(self, serializer):
+        """
+        Saves the cart item with a check to ensure the cart belongs to the current user.
+        """
+        cart = serializer.validated_data['cart']
+        if cart.user != self.request.user:
+            raise PermissionDenied("You do not have permission to add items to this cart.")
+        serializer.save()
